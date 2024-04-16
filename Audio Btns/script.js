06b1467d28bfd6e14d0fe2bc766984b768973cfd
Syed Shoebuddin <ushoeb25@gmail.com>
2024-04-16 10:13:37 +0530

    Updated audio sources in index.html and script.js

--- Audio Btns/script.js
+++ Audio Btns/script.js
@@ -1,35 +1,25 @@
-const img_btns= document.getElementById('btns');
-
+const img_btns = document.getElementById('btns');
 const screen = document.getElementById('container');
+const imgs = ['Nature', 'Moon', 'Sun', 'Pollution', 'Galaxy'];
+const audios = ['nature', 'moon', 'sun', 'pollution', 'galaxy'];
 
-const imgs= ['Nature', 'Moon', 'Sun', 'Pollution', 'Galaxy'];
-
-
-imgs.forEach(img => {
-    const i_btn= document.createElement('button');
-
+imgs.forEach((img, index) => {
+    const i_btn = document.createElement('button');
     i_btn.classList.add('i-btn');
     i_btn.innerText = img;
-
     img_btns.append(i_btn);
 
     i_btn.addEventListener('click', () => {
-        stopSong()
-        document.getElementById('heriye').play();
-    })
+        stopSong();
+        document.getElementById(audios[index]).play();
+    });
+
     img_btns.appendChild(i_btn);
 });
-
-
-function stopSong(){
-    imgs.forEach(img => {
-        const i_btn= document.createElement('button');
-
-        i_btn.classList.add('i-btn');
-        i_btn.innerText = img;
-        img_btns.append(i_btn);
-        img_btns.appendChild(i_btn);
-        document.getElementById('heriye').pause();
-        currentTime=0;         
+function stopSong() {
+    audios.forEach(audio => {
+        const audioElement = document.getElementById(audio);
+        audioElement.pause();
+        audioElement.currentTime = 0;
     });
 }
