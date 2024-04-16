868e97f8fead2410b3da1cff8e6804a4412b0acf
Syed Shoebuddin <ushoeb25@gmail.com>
2024-04-16 10:15:13 +0530

    Refactored toggle functionality and improved code readability in script.js

--- Clock/script.js
+++ Clock/script.js
@@ -14,41 +14,43 @@ const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', '
 
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-toggle.addEventListener('click', (e) =>{
-    const html =document.querySelector('html');
-    if(html.classList.contains('dark')){
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html');
+    if (html.classList.contains('dark')) {
         html.classList.remove('dark');
+        e.target.innerHTML = '';
         e.target.innerHTML = '<i class="fas fa-moon"></i>';
     } else {
         html.classList.add('dark');
+        e.target.innerHTML = '';
         e.target.innerHTML = '<i class="fas fa-sun"></i>';
     }
 });
 
-    //stackoverflow scale method.
-    const scale = (num, in_min, in_max, out_min, out_max) => {
-        return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-    }
+//stackoverflow scale method.
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
-function setTime(){
+function setTime() {
     let today = new Date();
 
     const month = today.getMonth();
     const day = today.getDay();
-    const  date= today.getDate();
+    const date = today.getDate();
 
     dateEl.innerHTML = `${days[day]}, ${months[month]}&nbsp;<span> ${date}</span>`;
-    
+
     const hours = today.getHours();
-    const minutes= today.getMinutes();
+    const minutes = today.getMinutes();
     const seconds = today.getSeconds();
-    
+
     hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hours, 0, 12, 0, 360)}deg)`;
     minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`;
     secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`;
 
-    const ampm= hours >12 ? 'PM' : 'AM';
-    timeEl.innerHTML= `${hours %12} : ${minutes < 10 ? `0${minutes}` : minutes}: ${seconds < 10 ? `0${seconds}` : seconds}&nbsp; ${ampm}`;
+    const ampm = hours > 12 ? 'PM' : 'AM';
+    timeEl.innerHTML = `${hours % 12} : ${minutes < 10 ? `0${minutes}` : minutes}: ${seconds < 10 ? `0${seconds}` : seconds}&nbsp; ${ampm}`;
 }
 setTime();
 setInterval(setTime, 1000);
